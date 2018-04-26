a2949844d926ec9529f7eca4f461546f1aa7b8c7
Jacob Joslin <mutantspew@gmail.com>
2018-04-25 22:57:41 -0400

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]	
+  get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
+
   resources :blogs
   
   root to: 'pages#home'
